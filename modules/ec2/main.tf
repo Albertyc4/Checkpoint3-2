8dad0dda638b3d75b3a9d0db08fa42ab05834f46
Albertyc4 <102705163+Albertyc4@users.noreply.github.com>
2022-10-02 21:55:26 -0300

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -94,54 +94,85 @@ resource "aws_launch_template" "template" {
   }
 }
 
-# AUTO SCALING GROUP
-resource "aws_autoscaling_group" "autoscal" {
-  name                = "autoscal"
-  vpc_zone_identifier = ["${var.sn_pub_1a_id}", "${var.sn_pub_1c_id}"]
-  desired_capacity    = var.desired_capacity
-  min_size            = var.min_size
-  max_size            = var.max_size
-  target_group_arns   = [aws_lb_target_group.targetg.arn]
-
-  launch_template {
-    id      = aws_launch_template.template.id
-    version = "$Latest"
-  }
-
-}
-
-# APPLICATION LOAD BALANCER
-resource "aws_lb" "loadb" {
-  name               = "loadb"
-  load_balancer_type = "application"
-  subnets            = [var.sn_pub_1a_id, var.sn_pub_1c_id]
-  security_groups    = [aws_security_group.sg_pub.id]
-
-  tags = {
-    Name = "loadb"
-  }
-}
-
 # APPLICATION LOAD BALANCER TARGET GROUP
-resource "aws_lb_target_group" "targetg" {
-  name     = "targetg"
+resource "aws_lb_target_group" "tg_app_notify" {
+  name     = "tg-app-notify"
   vpc_id   = var.vpc_id
   protocol = var.protocol
   port     = var.port
 
   tags = {
-    Name = "targetg"
+    Name = "tg_app_notify"
   }
 }
 
 # APPLICATION LOAD BALANCER LISTENER
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.loadb.arn
+resource "aws_lb_listener" "listener_app_notify" {
+  load_balancer_arn = aws_lb.elb_ws.arn
   protocol          = var.protocol
   port              = var.port
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.targetg.arn
+    target_group_arn = aws_lb_target_group.tg_app_notify.arn
+  }
+}
+
+# APPLICATION LOAD BALANCER
+resource "aws_lb" "elb_ws" {
+  name               = "elb-ws"
+  load_balancer_type = "application"
+  subnets            = [var.sn_vpc10_pub_1a_id, var.sn_vpc10_pub_1c_id]
+  security_groups    = [aws_security_group.vpc10_sg_pub.id]
+
+  tags = {
+    Name = "lb_app_notify"
+  }
+}
+
+# EC2 LAUNCH TEMPLATE
+data "template_file" "user_data" {
+  template = file("./app/userdata-notifier.sh")
+  vars = {
+    rds_endpoint = "${var.rds_endpoint}"
+    rds_user     = "${var.rds_user}"
+    rds_password = "${var.rds_password}"
+    rds_name     = "${var.rds_name}"
+  }
+}
+
+resource "aws_launch_template" "lt_check3" {
+  name                   = "lt_check3"
+  image_id               = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.vpc10_sg_pub.id]
+  key_name               = var.ssh_key
+  user_data              = base64encode(data.template_file.user_data.rendered)
+
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ws-"
+    }
+  }
+
+  tags = {
+    Name = "lt_check3"
+  }
+}
+
+# AUTO SCALING GROUP
+resource "aws_autoscaling_group" "asg_ws" {
+  name                = "asg_ws"
+  vpc_zone_identifier = ["${var.sn_vpc10_pub_1a_id}", "${var.sn_vpc10_pub_1c_id}"]
+  desired_capacity    = var.desired_capacity
+  min_size            = var.min_size
+  max_size            = var.max_size
+  target_group_arns   = [aws_lb_target_group.tg_app_notify.arn]
+
+  launch_template {
+    id      = aws_launch_template.lt_check3.id
+    version = "$Latest"
   }
 }
